4ca25257d6d2701032afacc3e59c6375798ba5b1
Moringa School <maryanneanyango98@gmail.com>
2022-09-02 16:13:16 +0300

    commit tree ac751d2874d5e71f0613a33c571445e82fb70e4b

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # end with .
+    self.end_with?(".")
   end
 
   def question?
+    # end with ?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    # end with !
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
   end
 end
